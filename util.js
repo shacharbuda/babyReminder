5d30522f3da58bd1caa5253170639c91934cafbc
Shachar Buda <shacharbuda27@gmail.com>
2019-12-24 15:10:25 +0200

    add util.js

---
+++ util.js
@@ -0,0 +1,14 @@
+function getJsonFromUrl(url) {
+  if(!url) url = location.search;
+  var query = url.substr(1);
+  var result = {};
+  query.split("&").forEach(function(part) {
+    var item = part.split("=");
+    result[item[0]] = decodeURIComponent(item[1]);
+  });
+  return result;
+}
+
+function paramsObjToUrl(params) {
+	return (Object.keys(params).map((paramKey) => `${paramKey}=${params[paramKey]}`).join('&'));
+}
