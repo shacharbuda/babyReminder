a68db5d55c96b4ecb174850242aaefa68f829121
Shachar Buda <shacharbuda27@gmail.com>
2020-01-05 10:01:16 +0200

    Updates

---
+++ precache-manifest.d42b91ff152e8707d363f113d3d7ed53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6942d1e69d483c3b81e36c985767ae22",
+    "url": "/babyReminder/index.html"
+  },
+  {
+    "revision": "8b83a8583c2e690fa392",
+    "url": "/babyReminder/static/css/main.ecab0f97.chunk.css"
+  },
+  {
+    "revision": "53ba8482286c0296e682",
+    "url": "/babyReminder/static/js/2.dba7c254.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/babyReminder/static/js/2.dba7c254.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b83a8583c2e690fa392",
+    "url": "/babyReminder/static/js/main.38a7fc20.chunk.js"
+  },
+  {
+    "revision": "0a3a42ca67ec9f649c05",
+    "url": "/babyReminder/static/js/runtime-main.4a2d1e2a.js"
+  }
+]);
