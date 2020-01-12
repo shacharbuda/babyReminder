2995cee152b58794379b2dac9192dc2263202a82
Shachar Buda <shacharbuda27@gmail.com>
2020-01-12 21:46:28 +0200

    Mui form

--- src/app/components/EditBaby/component.tsx
+++ src/app/components/EditBaby/component.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Baby, Reminder } from '../../interfaces';
-import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button } from '@material-ui/core'
+import { Dialog, DialogTitle, DialogContent, DialogContentText, TextField, DialogActions, Button, Checkbox, FormControlLabel } from '@material-ui/core'
 
 interface Props {
 	// From container
 	reminder: Reminder;
 	baby: Baby;
+	updateReminder: (isSeen: boolean) => void;
 	// ownProps
 	onClose: () => void;
 	isOpen: boolean;
@@ -16,18 +17,30 @@ interface State {
 }
 
 export class EditBabyComponent extends React.Component<Props, State> {	
+	fields: {label: string, value: string | number}[];
+	
 	constructor(props: Props) {
 		super(props);
-		const { baby: {seenReminders}, reminder } = props;
+		const { baby, reminder } = props;
+
+		const labels = [
+			"שם תינוק",
+			"תאריך לידה",
+			"גיל הופעת התזכורת (חודשים)",
+			"שם תזכורת"
+		];
+		const values = [baby.name, baby.birthdate.toISOString().slice(0,10), reminder.months, reminder.name];
+
+		this.fields = values.map((value, i) => ({label: labels[i], value}))
 
 		this.state = {
-			isSeen: seenReminders.includes(reminder.id)
+			isSeen: baby.seenReminders.includes(reminder.id)
 		}
 	}
 
 	handleSubmit = () => {
 		const isSeenChecked = this.state.isSeen;
-		(this.props as any).updateReminder(isSeenChecked);
+		this.props.updateReminder(isSeenChecked);
 
 		// Return to home.
 		this.closeModal();
@@ -39,6 +52,7 @@ export class EditBabyComponent extends React.Component<Props, State> {
 
 	render() {
 		const { isOpen, baby, reminder } = this.props;
+		const { isSeen } = this.state;
 
 		return (
 			<Dialog
@@ -47,43 +61,43 @@ export class EditBabyComponent extends React.Component<Props, State> {
 				onClose={this.closeModal}
 				onSubmit={this.handleSubmit}
 				 >
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        <DialogTitle id="form-dialog-title">תינוק {baby.name} - תזכורת {reminder.name}</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
+            כאן ניתן לערוך פרטי התזכורת. כרגע הכוונה היא רק ל'סיימתי עם התזכורת'.
           </DialogContentText>
-          <TextField
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-					<div className="form-group text-right text-info">
-						<label htmlFor="baby-name">שם תינוק</label>
-						<input type="text" value={baby.name} readOnly className="form-control" id="baby-name"/>
-					</div>
-					<div className="form-group text-right text-info">
-						<label htmlFor="baby-birthday">תאריך לידה</label>
-						<input type="date" readOnly value={baby.birthdate.toISOString().slice(0,10)} className="form-control" id="baby-birthday"/>
-					</div>
-					<div className="form-group text-right text-info">
-						<label htmlFor="reminder-months">גיל הופעת תזכורת (חודשים)</label>
-						<input type="text" readOnly value={reminder.months} className="form-control" id="reminder-months" aria-describedby="reminder-months-help"/>
-						<small id="reminder-months-help" className="form-text text-muted">בעתיד יהיה ניתן לערוך שדה זה.</small>
-					</div>
-					<div className="form-group text-right text-info">
-						<label htmlFor="reminder-name">שם תזכורת</label>
-						<input type="text" readOnly value={reminder.name} className="form-control" id="reminder-name"/>
-					</div>
-					<div className="form-check text-right text-info form-check">
-						<input checked={this.state.isSeen} onChange={({target}) => this.setState({isSeen: target.checked})} type="checkbox" className="form-check-input" id="reminder-seen"/>
-						<label className="form-check-label mr-3" htmlFor="reminder-seen">סיימתי עם התזכורת</label>
-					</div>
+					{
+						this.fields.map(f => (
+							<TextField
+								margin="normal"
+								label={f.label}
+								type="text"
+								value={f.value}
+								variant="outlined"
+								inputProps={{
+									readOnly: true,
+									disabled: true,
+									color: 'rgba(0, 0, 0, 0.87)'
+								}}
+								fullWidth
+							/>
+						))
+					}
+
+				<FormControlLabel
+					control={
+						<Checkbox
+							autoFocus
+							checked={isSeen}
+							onChange={({target}) => this.setState({isSeen: target.checked})}
+							color="primary"
+						/>
+					}
+					label="סיימתי עם התזכורת"
+				/>
         </DialogContent>
         <DialogActions>
-          <Button onClick={this.closeModal} color="primary">
+          <Button onClick={this.closeModal} color="secondary">
             בטל
           </Button>
           <Button onClick={this.handleSubmit} color="primary">
