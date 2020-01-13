cf41dbcba5e578a79debe978c6717de1434b17d0
Shachar Buda <shacharbuda27@gmail.com>
2020-01-13 16:24:48 +0200

    tbl container overflow

--- src/app/components/BabiesDataTable/component.tsx
+++ src/app/components/BabiesDataTable/component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Baby } from '../../interfaces';
 import { BabyDataRow } from '../BabyDataRow';
 import EditBabyContainer from '../EditBaby';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Theme, withStyles, WithStyles } from '@material-ui/core';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Theme, withStyles, WithStyles, createStyles } from '@material-ui/core';
 
 interface Props extends WithStyles<typeof styles> {
 	babies: Baby[];
@@ -31,16 +31,18 @@ class BabiesDataTableComponentA extends React.Component<Props, State> {
 	}
 	
 	render() {
+		const { classes } = this.props;
+
 		return (
 			<div className="h-100 w-100">
-			<TableContainer className="h-100 w-100">
+			<TableContainer className={classes.tblContainer}>
 				<Table aria-label="simple table">
 					<TableHead >
 						<TableRow>
-							<TableCell align="center" className={this.props.classes.head}>שם</TableCell>
-							<TableCell align="center" className={this.props.classes.head}>תאריך לידה</TableCell>
-							<TableCell align="center" className={this.props.classes.head}>גן ילדים</TableCell>
-							<TableCell align="center" className={this.props.classes.head}>תזכורת הבאה</TableCell>
+							<TableCell align="center" className={classes.head}>שם</TableCell>
+							<TableCell align="center" className={classes.head}>תאריך לידה</TableCell>
+							<TableCell align="center" className={classes.head}>גן ילדים</TableCell>
+							<TableCell align="center" className={classes.head}>תזכורת הבאה</TableCell>
 						</TableRow>
 					</TableHead>
 					<TableBody>
@@ -57,12 +59,18 @@ class BabiesDataTableComponentA extends React.Component<Props, State> {
 	}
 }
 
-const styles = (theme: Theme) => ({
+const styles = (theme: Theme) => createStyles({
 	head: {
 		backgroundColor: theme.palette.common.black,
 		color: theme.palette.common.white,
 		fontWeight: 1000,
 		fontSize: '1.2rem'
+	},
+	tblContainer: {
+		width: '100%',
+		height: '100%',
+		overflowX: 'hidden',
+		overflowY: 'auto'
 	}
 })
 
