70269b8e5c41edb4911ee0d9fd5905ca922664e8
Shachar Buda <shacharbuda27@gmail.com>
2020-01-13 09:55:19 +0200

    RTL app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import BabiesDataTableContainer from './components/BabiesDataTable';
 import { Baby } from './interfaces';
 import persistence from './utils/persistence';
+import RTL from './RTL';
 
 class App extends React.Component<{}, {isEditForm: boolean}> {
 	// No need of state as static.
@@ -15,20 +16,22 @@ class App extends React.Component<{}, {isEditForm: boolean}> {
 
   render() {
 		return (
-    <div className="App">
-			<header>
-				<h1 className="h1 text-center pt-5">תינוק תזכורת</h1>
-			</header>
-			<div className="container d-flex align-items-center body-content">
-				{
-					// Placeholder for router...
-					<BabiesDataTableContainer />
-				}
-			</div>
-			<footer className="text-center blockquote-footer" dir="ltr">
-				Made by Waffle
-			</footer>
-    </div>
+			<RTL>
+				<div className="App">
+					<header>
+						<h1 className="h1 text-center pt-5">תינוק תזכורת</h1>
+					</header>
+					<div className="container d-flex align-items-center body-content">
+						{
+							// Placeholder for router...
+							<BabiesDataTableContainer />
+						}
+					</div>
+					<footer className="text-center blockquote-footer" dir="ltr">
+						Made by Waffle
+					</footer>
+				</div>
+			</RTL>
   	);
 	}
 }
