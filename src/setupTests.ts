d7a9e01111d9daaaf680d21e26eea2fa20c196b8
Shachar Buda <shacharbuda27@gmail.com>
2020-01-25 21:55:51 +0200

    make tests stop annoying log printing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+global.console = {
+	error: () => {}
+};
+global.alert = () => {};
+global.prompt = () => {};
+global.matchMedia = () => ({});
+global.scrollTo = () => { };
