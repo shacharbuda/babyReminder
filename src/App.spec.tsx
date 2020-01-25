f5a6d9afc7eb74d87026d85e7a67eba9e4ac80e2
Shachar Buda <shacharbuda27@gmail.com>
2020-01-25 21:56:26 +0200

    rename app test to app spec

---
+++ src/App.spec.tsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+// // import { render } from '@testing-library/react';
+// import App from './app/App';
+// import { Provider } from 'react-redux';
+// import store from './app/store';
+
+it('renders learn react link', () => {
+	expect(1===1);
+});
--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './app/App';
-import { Provider } from 'react-redux';
-import store from './app/store';
-
-test('renders learn react link', () => {
-	expect(1===1);
-});
