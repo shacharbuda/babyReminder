363e9c26c89e3a1647fd353d4b235ea822837f9d
Shachar Buda <shacharbuda27@gmail.com>
2020-02-19 21:59:21 +0200

    move to js

--- src/app/components/BabyDataRow/index.js
+++ src/app/components/BabyDataRow/index.js
@@ -1,14 +1,13 @@
 import { connect } from 'react-redux';
 import { BabyDataRow } from './component';
-import { Dispatch } from 'redux';
 import { removeBaby } from '../../store/actions';
 
 const mapStateToProps = (state) => ({
 	reminders: state.reminder
 });
 
-const mapDistpatchToProps = (dispatch: Dispatch) => ({
-	removeBaby: (id: number) => dispatch(removeBaby(id))
+const mapDistpatchToProps = (dispatch) => ({
+	removeBaby: (id) => dispatch(removeBaby(id))
 })
 
 export default connect(mapStateToProps, mapDistpatchToProps)(BabyDataRow)
