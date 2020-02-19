612cbd3f06e1e2a768349f934637c4f2b864722c
Shachar Buda <shacharbuda27@gmail.com>
2020-02-20 00:36:31 +0200

    todo

--- src/app/components/EditBaby/index.ts
+++ src/app/components/EditBaby/index.ts
@@ -12,6 +12,7 @@ interface Props {
 };
 
 const mapStateToProps = (state, ownProps: Props) => ({
+	// TODO: get from db. delete all state!
 	baby: state.baby.find(b => b.id === ownProps.pickedBabyReminder.babyId),
 	reminder: state.reminder.find(r => r.id === ownProps.pickedBabyReminder.reminderId)
 });
