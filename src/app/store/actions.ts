10b00c3d1172534baef8973b0b5931aac9d04136
Shachar Buda <shacharbuda27@gmail.com>
2020-02-21 13:04:19 +0200

    db operations on action creators!!

--- src/app/store/actions.ts
+++ src/app/store/actions.ts
@@ -1,69 +1,68 @@
-import { ActionCreator, Action } from 'redux';
-import { BabyNew } from '../interfaces';
+import { BabyNew, BabyReminder } from '../interfaces';
+import { FirebaseFirestore } from '@firebase/firestore-types'
+
+type DBFunction = (firestore: FirebaseFirestore) => {};
 
 export const ACTION_TYPES = {
-	INIT_BABIES: 'INIT_BABIES',
-	ADD_REMINDER: 'ADD_REMINDER',
-	REMOVE_REMINDER: 'REMOVE_REMIDNER',
-	ADD_BABY: 'ADD_BABY',
-	REMOVE_BABY: 'REMOVE_BABY',
-	SORT_BABIES: 'SORT_BABIES'
+	SET_LOADING: 'SET_LOADING',
+	SET_ERROR: 'SET_ERROR',
+	SET_SUCCESS: 'SET_SUCCESS'
 };
 
-export const initBabies: ActionCreator<Action> = () => ({
-  type: ACTION_TYPES.INIT_BABIES
-});
+// isGlobal can be used for spinner on root app.
+const setLoading = (isGlobal = false) => ({
+	type: ACTION_TYPES.SET_LOADING,
+	payload: { isGlobal }
+})
 
-export const addBaby = (newBaby: BabyNew) => async (dispatch, getState, { getFirebase, getFirestore }) => {
+const setSuccess = (msg = null) => ({
+	type: ACTION_TYPES.SET_SUCCESS,
+	payload: { msg }
+})
+
+const setError = (msg = null) => ({
+	type: ACTION_TYPES.SET_ERROR,
+	payload: { msg }
+})
+
+const dbOperation = (dbFunction: DBFunction, successMsg?: string, errMsg?: string, isGlobal?: boolean) => async (dispatch, getState, { getFirebase, getFirestore }) => {
 	try {
+		dispatch(setLoading(isGlobal));
+		await dbFunction(getFirestore())
+		dispatch(setSuccess(successMsg));
+	} catch(e) {
+		dispatch(setError(errMsg));
+	}
+}
 
-		const firestore = getFirestore();
+export const addBaby = (newBaby: BabyNew) => {
+	const dbFunction: DBFunction = async (firestore) => {
 		await firestore.collection('babies').add({
 			...newBaby
 		});
-		console.log('success!');
-		// dispatch({	
-		// 	type: ACTION_TYPES.ADD_BABY,
-		// 	payload: {
-		// 		newBaby
-		// 	}
-		// });
-	} catch(e) {
-		console.error('e ? ', e);
 	}
-}
+	const successMsg = "Added baby successfuly!";
+	const errMsg = "Error adding baby...";
+	const isGlobal = true;
 
-export const removeBaby = (babyId: number) => async (dispatch, getState, { getFirebase, getFirestore }) => {
-	try {
-		console.log('babyId ?', babyId);
-		const firestore = getFirestore();
+	return dbOperation(dbFunction, successMsg, errMsg, isGlobal);
+};
+
+export const removeBaby = (babyId: string) => {
+	const dbFunction: DBFunction = async (firestore) => {
 		await firestore.collection('babies').doc(babyId).delete();
-		console.log('success!');
-		// dispatch({
-		// 	type: ACTION_TYPES.REMOVE_BABY,
-		// 	payload: {
-		// 		babyId
-		// 	}
-		// });
-	} catch(e) {
-		console.error('e ? ', e);
 	}
+	return dbOperation(dbFunction);
 }
 
-export const sortBabies: ActionCreator<Action> = () => ({
-	type: ACTION_TYPES.SORT_BABIES
-});
+export const addReminder = (payload: BabyReminder) => {
+	console.log(`this should add reminder ${payload.reminderId} to baby ${payload.babyId}`);
+	// type: ACTION_TYPES.ADD_REMINDER,
+	// payload
+};
 
-interface BabyReminder {
-	babyId: number;
-	reminderId: number;
-}
-export const addReminder = (payload: BabyReminder) => ({
-	type: ACTION_TYPES.ADD_REMINDER,
-	payload
-});
-
-export const removeReminder = (payload: BabyReminder) => ({
-	type: ACTION_TYPES.REMOVE_REMINDER,
-	payload
-});
+export const removeReminder = (payload: BabyReminder) => {
+	console.log(`this should remove reminder ${payload.reminderId} from baby ${payload.babyId}`);
+	// type: ACTION_TYPES.REMOVE_REMINDER,
+	// payload
+};
