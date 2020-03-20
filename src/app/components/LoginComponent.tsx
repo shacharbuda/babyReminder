a3ea9081dc7f82e323d9095905e449686ef11db8
Shachar Buda <shacharbuda27@gmail.com>
2020-03-20 12:40:38 +0200

    style

--- src/app/components/LoginComponent.tsx
+++ src/app/components/LoginComponent.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Dialog, DialogTitle, DialogContent, CircularProgress } from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, CircularProgress, Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default class LoginComponent extends React.Component<{}, {}> {
-
-	render() {
-		return (
-			<Dialog
-				aria-labelledby="form-dialog-title"
-				open={true}
-				 >
-        <DialogTitle id="form-dialog-title">אנא המתיני להתחברות...</DialogTitle>
-        <DialogContent>
-					<div style={{display: 'flex', justifyContent: 'center'}}>
-						<CircularProgress/>
-					</div>
-				</DialogContent>
-      </Dialog>
-			);
+const useStyles = makeStyles((theme: Theme) => ({
+	content: {
+		padding: theme.spacing(3)
+	},
+	spinner: {
+		display: 'flex',
+		justifyContent: 'center'
 	}
+}));
+
+
+function LoginComponent(props) {
+	// @ts-ignore-nextline
+	const classes: any = useStyles();
+
+	return (
+		<Dialog
+			aria-labelledby="form-dialog-title"
+			open={true}
+				>
+			<DialogTitle id="form-dialog-title">אנא המתיני להתחברות...</DialogTitle>
+			<DialogContent className={classes.content}>
+				<div className={classes.spinner}>
+					<CircularProgress/>
+				</div>
+			</DialogContent>
+		</Dialog>
+		);
 }
+
+export default LoginComponent;
