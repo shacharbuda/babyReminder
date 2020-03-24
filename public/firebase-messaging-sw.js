c520fa15ecb4097f78a2779bd802b2edd0d95ecb
Shachar Buda <shacharbuda27@gmail.com>
2020-03-24 20:28:38 +0200

    bg message

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js");
+// importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js");
+
+firebase.initializeApp({
+  // TODO: env var
+	messagingSenderId: "864184464261"
+})
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(payload => {
+  const notification = JSON.parse(payload.data.notification);
+  const notificationTitle = notification.title;
+  const notificationOptions = {
+    body: notification.body
+  };
+  //Show the notification :)
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
