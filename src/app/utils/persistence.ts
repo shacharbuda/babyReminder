9a0f3386614693eba3641cf103a96e55c0ea8e40
Shachar Buda <shacharbuda27@gmail.com>
2020-04-15 21:25:30 +0300

    spaces

--- src/app/utils/persistence.ts
+++ src/app/utils/persistence.ts
@@ -1,32 +1,32 @@
 import consts from './constants';
 
 export const PERSISTENCE_CODES = {
-	VERSION: 'VERSION',
+  VERSION: 'VERSION',
 }
 
 function handleVersion() {
-	if (isNewVersion()) {
+  if (isNewVersion()) {
     // Alert user for new version!
-		alert('גרסה חדשה!');
+    alert('גרסה חדשה!');
 
-		updateUserVersion();
-	}
+    updateUserVersion();
+  }
 }
 
 function isNewVersion() {
-	// New version check
-	const serverVersion = consts.APP_VERSION;
-	const userVersion = localStorage.getItem(PERSISTENCE_CODES.VERSION);
-	return (!userVersion || userVersion !== serverVersion);
+  // New version check
+  const serverVersion = consts.APP_VERSION;
+  const userVersion = localStorage.getItem(PERSISTENCE_CODES.VERSION);
+  return (!userVersion || userVersion !== serverVersion);
 }
 
 function updateUserVersion() {
-	const serverVersion = consts.APP_VERSION;
-	localStorage.setItem(PERSISTENCE_CODES.VERSION, serverVersion);
+  const serverVersion = consts.APP_VERSION;
+  localStorage.setItem(PERSISTENCE_CODES.VERSION, serverVersion);
 }
 
 export default {
-	isNewVersion,
-	updateUserVersion,
-	handleVersion,
+  isNewVersion,
+  updateUserVersion,
+  handleVersion,
 };
