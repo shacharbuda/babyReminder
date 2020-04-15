10674c49fc79b677f405fc3aadd0c8e8b76527bf
Shachar Buda <shacharbuda27@gmail.com>
2020-04-15 21:22:17 +0300

    unused consts

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,5 +1,3 @@
-export const EDIT_FORM_URL = 'editBabyReminder.html';
-export const HOME_URL = 'index.html';
 export const IS_DEV = process.env.NODE_ENV !== `production`;
 
 export const COLLECTIONS = {
@@ -9,7 +7,6 @@ export const COLLECTIONS = {
 
 export  default {
 	APP_VERSION: process.env.REACT_APP_VERSION,
-	VALID_USERNAME: 'אורי',
 	IS_DEV,
 	COLLECTIONS
 }
