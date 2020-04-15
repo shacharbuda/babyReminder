5d9a56bd95255b0cb41d4fc99bd38735c0d67cdc
Shachar Buda <shacharbuda27@gmail.com>
2020-04-15 22:04:28 +0300

    AppContainer dir

--- src/app/AppContainer/AppContainer.tsx
+++ src/app/AppContainer/AppContainer.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import './App.css';
-import BabiesDataTableContainer from './components/BabiesDataTable';
-import RTL from './RTL';
-import consts from './utils/constants';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
-import firebase, { analytics, authUser } from '../config/fbConfig';
-import LoginComponent from './components/LoginComponent';
+import consts from '../utils/constants';
+// import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import BabiesDataTableContainer from '../components/BabiesDataTable';
+// import DateFnsUtils from '@date-io/date-fns';
+import LoginComponent from '../components/LoginComponent';
 import { ExitToApp as LogoutIcon } from '@material-ui/icons';
-import { connect } from 'react-redux';
 import { Fab } from '@material-ui/core';
-import util from './utils/util';
-import { isLoaded } from 'react-redux-firebase';
-import persistence from './utils/persistence';
+import util from '../utils/util';
 
 interface Props {
   isLoggedIn: boolean;
   isLoading: boolean;
   displayName: string;
+  authUser: () => void;
+  signOut: () => void;
+  analytics: {
+    mount: () => void,
+    fotterClick: () => void
+  };
+  handleVersion: () => void;
 }
 
-class App extends React.Component<Props, {}> {
+class AppContainer extends React.Component<Props, {}> {
   componentDidMount() {
-    analytics('APP_MOUNTED');
-    persistence.handleVersion();
+    const { analytics, handleVersion } = this.props;
+    analytics.mount();
+    handleVersion();
   }
 
   componentDidUpdate(prevProps: Props) {
-    const { isLoggedIn, isLoading } = this.props; 
+    const { isLoggedIn, isLoading, authUser } = this.props; 
     
     // Nothing to do here if still loading..
     if (isLoading) return;
@@ -39,7 +42,7 @@ class App extends React.Component<Props, {}> {
   }
 
   render() {
-    const { isLoading, isLoggedIn, displayName } = this.props;
+    const { isLoading, isLoggedIn, displayName, analytics, signOut } = this.props;
 
     if (isLoading || !isLoggedIn) {
       return (		
@@ -47,18 +50,19 @@ class App extends React.Component<Props, {}> {
       )
     }
 
-    return (
-      <RTL>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    return (      
           <div className="App">
+            {/* TODO: component */}
             <header className="container d-flex justify-content-between">
               <h1 className="m-auto text-center">תינוק תזכורת</h1>
               <h5 className="m-auto text-center">{util.getTimeInDayGreet()}<br />{displayName}!</h5>
             </header>
+            {/* TODO: component */}
             <div className="container d-flex align-items-center body-content">
                 <BabiesDataTableContainer />
             </div>
-            <footer onClick={() => analytics('CLICK_ON_FOOTER')} className="ltr text-center blockquote-footer">
+            {/* TODO: component */}
+            <footer onClick={analytics.fotterClick} className="ltr text-center blockquote-footer">
               Made by Waffle (v{consts.APP_VERSION})
             </footer>
             {
@@ -66,22 +70,15 @@ class App extends React.Component<Props, {}> {
               <div
                 style={{ position: 'fixed', bottom: '1em', right: '1em'}}
               >
-                <Fab onClick={() => firebase.auth().signOut()} color="secondary" aria-label="edit">
+                {/* TODO: component */}
+                <Fab onClick={signOut} color="secondary" aria-label="edit">
                   <LogoutIcon />
                 </Fab>
               </div>
             }
-          </div>
-        </MuiPickersUtilsProvider>
-      </RTL>
+        </div>
     );
   }
 }
 
-const mapStateToProps = (state: any) => ({
-  isLoading: !isLoaded(state.firebase.auth),
-  isLoggedIn: !state.firebase.auth.isEmpty,
-  displayName: state.firebase.auth.displayName
-})
-
-export default connect(mapStateToProps)(App);
+export default AppContainer;
---
+++ src/app/AppContainer/index.ts
@@ -0,0 +1,25 @@
+import { analytics, authUser, signOut } from '../../config/fbConfig';
+import { connect } from 'react-redux';
+import { isLoaded } from 'react-redux-firebase';
+import AppComponent from './AppContainer';
+import persistence from '../utils/persistence'
+
+const mapStateToProps = (state: any) => ({
+  isLoading: !isLoaded(state.firebase.auth),
+  isLoggedIn: !state.firebase.auth.isEmpty,
+  displayName: state.firebase.auth.displayName
+});
+
+// Those aren't related to store but still passed as I really wanted to use
+// the container structure like this. If you don't like it, well.. That's a bummer.
+const propsToPass = () => ({
+  authUser,
+  signOut,
+  analytics: {
+    mount: analytics('APP_MOUNTED'),
+    fotterClick: analytics('CLICK_ON_FOOTER')
+  },
+  handleVersion: persistence.handleVersion
+});
+
+export default connect(mapStateToProps, propsToPass)(AppComponent);
