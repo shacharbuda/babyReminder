65a43b07bf4b5c1dc776d9eadc476d8dd7763a1b
Shachar Buda <shacharbuda27@gmail.com>
2020-04-18 21:50:33 +0300

    IS_DEV configs

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -1,4 +1,5 @@
-export const IS_DEV = process.env.NODE_ENV !== `production`;
+export const IS_DEV = process.env.NODE_ENV !== 'production'
+                    || process.env.PUBLIC_URL.includes('dev');
 
 export const COLLECTIONS = {
 	BABIES: 'babies',
