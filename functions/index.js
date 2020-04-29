96dcd0843e191cb00c4ccb6f24168237d78875f2
Shachar Buda <shacharbuda27@gmail.com>
2020-04-29 11:38:53 +0300

    stub for real function - sendMessageOnReminder

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const moment = require('moment');
+
+const TIME_ZONE = 'Asia/Jerusalem';
 
 admin.initializeApp({
   // TODO: Man how do i get dev or prod?
@@ -14,9 +17,13 @@ const db = functions.firestore;
 exports.helloWorld = db.document('users/{userId}')
   .onWrite((async (change, context) => {
   console.log('helloWorld()');
-  console.log('context.after.data() ?', context.after.data());
-  const message = 'Hi there this is a test';
+  // console.log('context.after.data() ?', context.after.data());
+  await sendMessageToAllUsers();
 
+  console.log('DONE helloWorld()');
+}));
+
+const sendMessageToAllUsers = async (message="test message") => {
   try {
     const { docs } = await dbAdmin.collection(`users`).get();
     const payload = {
@@ -34,13 +41,24 @@ exports.helloWorld = db.document('users/{userId}')
       await admin.messaging().sendToDevice(token, payload);
       console.log(`Successfully sent message to user ${doc.get('displayName')}`);
     });
-
-    console.log("Hello from Firebase!");
   } catch(err) {
     console.error('error ?', err);
     return;
   }
-}));
+}
+
+// BIG PROBLEM - To execute a scheduled function, we need to enable Billing...
+// exports.scheduledFunction = functions.pubsub.schedule('every day at 11:00am')
+//   .timeZone(TIME_ZONE)
+//   .onRun(async (context) => {
+//     const now = moment().format('MMMM Do, h:mm:ss');
+//     console.log('scheduledFunction() Running.. Time is ' + now);
+//     // Here we'll write the function to check if there are reminders for the authenitacted user
+//     // that aren't seen yet. If there are, we'll send a message to the user.
+//     await sendMessageToAllUsers(now);
+//     console.log('End of scheduledFunction()');
+//     return null;
+// });
 
 
 async function asyncForEach(array, callback) {
